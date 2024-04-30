dfb8395d863bf573b28fadc202c3a787bdc13d57
sathitkul12 <sathitkuy123@gmail.com>
2024-04-30 20:07:18 +0700

    commit

--- lib/screen/mqtt/mqtt.dart
+++ lib/screen/mqtt/mqtt.dart
@@ -8,11 +8,11 @@ class MqttHandler {
   // Constants for MQTT topics and client ID
   final String mqttServer = 'test.mosquitto.org';
   final String clientId = 'clientId-bzwIkQ3vF5';
-  final String tempTopic = 'esp32/temp';
-  final String humidityTopic = 'esp32/humidity';
-  final String ldrTopic = 'esp32/ldr';
-  final String mqTopic = 'esp32/mq';
-  final String soilTopic = 'esp32/soil';
+  // final String tempTopic = 'esp32/temp';
+  // final String humidityTopic = 'esp32/humidity';
+  // final String ldrTopic = 'esp32/ldr';
+  // final String mqTopic = 'esp32/mq';
+  // final String soilTopic = 'esp32/soil';
   final String autoModeTopic = 'esp32/auto_mode';
 
   // MQTT client and StreamControllers for different sensor data
@@ -44,30 +44,30 @@ class MqttHandler {
   // Set up MQTT connection and subscriptions
   void _setupMqtt() {
     client.logging(on: false);
-    client.onConnected = onConnected;
+    // client.onConnected = onConnected;
     client.onDisconnected = onDisconnected;
     client.onUnsubscribed = onUnsubscribed;
     client.connect();
   }
 
   // Callback on successful MQTT connection
-  void onConnected() {
-    // Subscribe to different sensor topics
-    client.subscribe(tempTopic, MqttQos.atLeastOnce);
-    client.subscribe(humidityTopic, MqttQos.atLeastOnce);
-    client.subscribe(ldrTopic, MqttQos.atLeastOnce);
-    client.subscribe(mqTopic, MqttQos.atLeastOnce);
-    client.subscribe(soilTopic, MqttQos.atLeastOnce);
+  // void onConnected() {
+  //   // Subscribe to different sensor topics
+  //   client.subscribe(tempTopic, MqttQos.atLeastOnce);
+  //   client.subscribe(humidityTopic, MqttQos.atLeastOnce);
+  //   client.subscribe(ldrTopic, MqttQos.atLeastOnce);
+  //   client.subscribe(mqTopic, MqttQos.atLeastOnce);
+  //   client.subscribe(soilTopic, MqttQos.atLeastOnce);
 
-    // Listen for incoming messages and handle them
-    client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-      final MqttPublishMessage message = c[0].payload as MqttPublishMessage;
-      final String payload =
-          MqttPublishPayload.bytesToStringAsString(message.payload.message);
-      final String topic = c[0].topic;
-      handleMessage(topic, payload);
-    });
-  }
+  // Listen for incoming messages and handle them
+  //   client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+  //     final MqttPublishMessage message = c[0].payload as MqttPublishMessage;
+  //     final String payload =
+  //         MqttPublishPayload.bytesToStringAsString(message.payload.message);
+  //     final String topic = c[0].topic;
+  //     handleMessage(topic, payload);
+  //   });
+  // }
 
   // Callback on MQTT disconnection
   void onDisconnected() {
@@ -77,27 +77,27 @@ class MqttHandler {
   // Callback on unsubscribing from a topic
   void onUnsubscribed(String? topic) {}
 
-  // Handle incoming MQTT messages based on topic
-  void handleMessage(String topic, String payload) {
-    double value = double.tryParse(payload) ?? 0.0;
+  // // Handle incoming MQTT messages based on topic
+  // void handleMessage(String topic, String payload) {
+  //   double value = double.tryParse(payload) ?? 0.0;
 
-    // Update respective StreamControllers based on topic
-    if (topic == tempTopic) {
-      _temperatureStreamController.add(value);
-    }
-    if (topic == humidityTopic) {
-      _humidityStreamController.add(value);
-    }
-    if (topic == ldrTopic) {
-      _ldrStreamController.add(value);
-    }
-    if (topic == mqTopic) {
-      _mqStreamController.add(value);
-    }
-    if (topic == soilTopic) {
-      _soilStreamController.add(value);
-    }
-  }
+  //   // Update respective StreamControllers based on topic
+  //   if (topic == tempTopic) {
+  //     _temperatureStreamController.add(value);
+  //   }
+  //   if (topic == humidityTopic) {
+  //     _humidityStreamController.add(value);
+  //   }
+  //   if (topic == ldrTopic) {
+  //     _ldrStreamController.add(value);
+  //   }
+  //   if (topic == mqTopic) {
+  //     _mqStreamController.add(value);
+  //   }
+  //   if (topic == soilTopic) {
+  //     _soilStreamController.add(value);
+  //   }
+  // }
 
   // Control relay by sending MQTT command
   void controlRelay(String topic, String command) {
